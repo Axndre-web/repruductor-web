5a611d1cd0b8fc1f2bca1aee54240a9e1142d009
Axndre-web <steven.ax115@gmail.com>
2026-08-21 16:43:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pulse-player-v1';
+const CACHE_NAME = 'pulse-player-v2';
 const APP_SHELL = ['/', '/index.html', '/style.css', '/script.js', '/manifest.webmanifest', '/icon.svg'];
 
 self.addEventListener('install', (event) => {
